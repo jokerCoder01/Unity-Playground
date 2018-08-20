1e338b98cbcbb1d41f92caf767d0186b7885a847
jokerCoder01 <jokercoder01@gmail.com>
2018-08-20 18:51:07 +0300

    Test Github Plugin for Unity

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * 
+ */
+ 
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	#region Public Fields
+    #endregion
+	
+	#region Private Fields
+    #endregion
+	
+	#region Properties
+    #endregion
+
+	#region Unity Methods
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	#endregion
+	
+	#region Private Methods
+    #endregion
+}
